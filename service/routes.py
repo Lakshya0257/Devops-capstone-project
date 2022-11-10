df01bf60e7bbd598424b87885c29e1eaafcd20c1
raigo276 <rainel.gonzalez@web.de>
2022-11-10 18:48:37 -0500

    Delete Account

--- service/routes.py
+++ service/routes.py
@@ -117,7 +117,17 @@ def update_account(id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:id>", methods=["DELETE"])
+def delete_accounts(id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", id)
+    account = Account.find(id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -169,3 +169,9 @@ class TestAccountService(TestCase):
         """ It should return not found if account does not exists while updating"""
         response = self.client.put(f"{BASE_URL}/0", json=None)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_account(self):
+        """ It should delete an account if found """
+        account = self._create_accounts(1)[0]
+        response = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
